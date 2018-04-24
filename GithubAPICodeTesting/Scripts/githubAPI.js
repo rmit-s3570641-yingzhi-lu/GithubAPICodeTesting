ff72e268134c160f0067fd2987ec247071785e29
Shallowpig <s3570641@student.rmit.edu.au>
2018-04-24 23:59:44 +1000

    Write css to show the frontend with Bootstrap AJAX api js file is ready layout is done

---
+++ GithubAPICodeTesting/Scripts/githubAPI.js
@@ -0,0 +1,19 @@
+﻿/*global $, window*/
+/*jslint maxerr: 50, browser: true, devel: true, multivar: true */
+/*jshint maxparams:10, maxdepth:5, maxstatements:40, maxcomplexity:15 */
+
+/*jslint
+    this
+*/
+(function (window) {
+    "use strict";
+
+    function search() {
+
+    }
+
+    window.onload = function () {
+        search();
+    };
+
+}(window));
